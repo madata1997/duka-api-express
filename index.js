d8503b5adac04b329ddcc826da448da3fd959126
madata1997 <mussamadata5@gmail.com>
2026-04-14 09:42:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('PDF'));
 
-// ✅ Add this
+//  Add this
 app.get('/', (req, res) => {
   res.send('API is running 🚀');
 });
