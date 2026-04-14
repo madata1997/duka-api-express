ae3cd3186df590562fa2e5bfebf1d624532f4ebf
madata1997 <mussamadata5@gmail.com>
2026-04-14 13:24:18 +0300

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,30 +1,28 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
-const date = require('date-and-time');
-
-const now = new Date();
 
 let connection;
 
-(async () => {
+const getConnection = async () => {
   try {
-    connection = await mysql.createConnection({
-      port: process.env.DB_PORT,
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      waitForConnections: true,
-      connectionLimit: 10,
-      queueLimit: 0
-    });
+    if (!connection) {
+      connection = await mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT
+      });
 
-    console.log('Connected to the database at', date.format(now, 'YYYY-MM-DD HH:mm:ss'));
+      console.log(' Database connected');
+    }
+
+    return connection;
+
   } catch (err) {
-    console.error('Failed to connect to the database:', err.message);
+    console.error(' DB Connection Error:', err.message);
+    throw err;
   }
-})();
-
-module.exports = {
-  getConnection: async () => connection
 };
+
+module.exports = { getConnection };
